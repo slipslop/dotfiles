5c031a1b1b598de487ba7d6163002f854398593d
slipslop <vili.takaanttila@gmail.com>
2023-12-19 09:47:23 +0200

    Use lazy-vim instead of packer

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -1,50 +1,24 @@
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
-local packer_bootstrap = ensure_packer()
-
-require('packer').reset()
-require('packer').init({
-    compile_path = vim.fn.stdpath('data') .. '/site/plugin/packer_compiled.lua',
+require("lazy").setup({
+    "rebelot/kanagawa.nvim",
+    "tpope/vim-commentary",
+    "sheerun/vim-polyglot",
+    "farmergreg/vim-lastplace",
+    {
+        'windwp/nvim-autopairs',
+        event = "InsertEnter",
+        opts = {}
+    }
 })
-
-local use = require('packer').use
-
-use('wbthomason/packer.nvim')
-
--- Commenting support.
-use('tpope/vim-commentary')
-
--- Add more languages
-use('sheerun/vim-polyglot')
-
--- Jump to the last location when opening a file
-use('farmergreg/vim-lastplace')
-
-use({
-  'windwp/nvim-autopairs',
-  config = function()
-    require('nvim-autopairs').setup()
-  end,
-})
-
--- Automatically set up your configuration after cloning packer.nvim
--- Put this at the end after all plugins
-if packer_bootstrap then
-    require('packer').sync()
-end
-
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile>
-  augroup end
-]])
