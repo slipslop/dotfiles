1864de4c67c583115aab8a6df0a2e249bf9bbc93
slipslop <vili.takaanttila@gmail.com>
2024-05-03 10:26:52 +0300

    Add new keymaps to neovim

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -1,11 +1,23 @@
+local keymap = vim.keymap
+
 -- map k and j to gk and gj, in normal mode, if count is 0:
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true })
+keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true })
+keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true })
 
 -- Reselect the visual after indenting
-vim.keymap.set('v', '<', '<gv')
-vim.keymap.set('v', '>', '>gv')
+keymap.set('v', '<', '<gv')
+keymap.set('v', '>', '>gv')
 
 -- Maintain the cursor position when yanking a visual selection
-vim.keymap.set('v', 'y', 'myy`y')
+keymap.set('v', 'y', 'myy`y')
 
+keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split vertically" })
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split horizontally" })
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close split" })
+
+keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to upper split" })
+keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to split below" })
+keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to split right" })
+keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to split left" })
