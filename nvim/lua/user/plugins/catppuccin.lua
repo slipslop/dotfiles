64509048d2f09e7ad8a7f4f6a8abe80b56ac0154
slipslop <vili.takaanttila@gmail.com>
2024-05-22 13:40:43 +0300

    Temporarily fix cattpuccin theme

--- nvim/lua/user/plugins/catppuccin.lua
+++ nvim/lua/user/plugins/catppuccin.lua
@@ -1,4 +1,6 @@
 require("catppuccin").setup({
     flavour = "macchiato",
 })
-vim.cmd.colorscheme = "catppuccin"
+
+vim.cmd.colorscheme "catppuccin"
+vim.cmd.colorscheme "vim" -- restore nvim 0.9.5 functionality. TODO: remove when catppuccin is updated
