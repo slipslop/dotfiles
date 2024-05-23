d10acc10f9c59a6d4ffc0631afb4a886bfdbd995
slipslop <vili.takaanttila@gmail.com>
2024-05-23 11:57:22 +0300

    Reorder lines in catppuccin

--- nvim/lua/user/plugins/catppuccin.lua
+++ nvim/lua/user/plugins/catppuccin.lua
@@ -2,5 +2,5 @@ require("catppuccin").setup({
     flavour = "macchiato",
 })
 
-vim.cmd.colorscheme "catppuccin"
 vim.cmd.colorscheme "vim" -- restore nvim 0.9.5 functionality. TODO: remove when catppuccin is updated
+vim.cmd.colorscheme "catppuccin"
