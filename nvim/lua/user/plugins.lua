00d5022ee7aa551256dd666bcf6ebc1d86fc4aa5
slipslop <vili.takaanttila@gmail.com>
2024-05-28 14:39:53 +0300

    Install cmp and LuaSnip

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -86,4 +86,19 @@ require("lazy").setup({
             require("user/plugins/mason-lspconfig")
         end,
     },
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            'neovim/nvim-lspconfig',
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-path',
+            'hrsh7th/cmp-cmdline',
+            'L3MON4D3/LuaSnip',
+            'saadparwaiz1/cmp_luasnip',
+        },
+        config = function()
+            require("user/plugins/nvim-cmp")
+        end,
+    },
 })
