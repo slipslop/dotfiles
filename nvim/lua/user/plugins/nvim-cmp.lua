d090fa827b78875e2076ecb4f76b4dba0b768bee
slipslop <vili.takaanttila@gmail.com>
2024-05-28 15:49:49 +0300

    Map ctrl+k to mapping.complete

--- nvim/lua/user/plugins/nvim-cmp.lua
+++ nvim/lua/user/plugins/nvim-cmp.lua
@@ -14,13 +14,12 @@ cmp.setup({
     mapping = cmp.mapping.preset.insert({
       ['<C-b>'] = cmp.mapping.scroll_docs(-4),
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-k>'] = cmp.mapping.complete(),
       ['<C-e>'] = cmp.mapping.abort(),
       ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     }),
     sources = cmp.config.sources({
       { name = 'nvim_lsp' },
-      { name = 'vsnip' }, -- For vsnip users.
       { name = 'luasnip' }, -- For luasnip users.
     }, {
       { name = 'buffer' },
