f2b75d82ee9bda745afb1441f4c55b5adc035dc3
slipslop <vili.takaanttila@gmail.com>
2024-06-04 09:49:54 +0300

    Update find_files' find_command

--- nvim/lua/user/plugins/telescope.lua
+++ nvim/lua/user/plugins/telescope.lua
@@ -1,3 +1,10 @@
+require('telescope').setup({
+    pickers = {
+        find_files = {
+            find_command = { 'rg', '--files', '--hidden', '--no-ignore', '--glob', '!.git/', '--glob', '!node_modules/', '--glob', '!*.svg' }
+        },
+    },
+})
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
