472d78960ee7edc1b4f9e575c2487c7a34c8ab51
slipslop <vili.takaanttila@gmail.com>
2024-08-19 16:26:00 +0300

    Update keymaps

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -1,5 +1,7 @@
 local keymap = vim.keymap
 
+keymap.set("n", "<leader>r", ":source ~/.config/nvim/init.lua<CR>")
+
 -- map k and j to gk and gj, in normal mode, if count is 0:
 keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true })
 keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true })
@@ -11,12 +13,19 @@ keymap.set('v', '>', '>gv')
 -- Maintain the cursor position when yanking a visual selection
 keymap.set('v', 'y', 'myy`y')
 
-keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split vertically" })
-keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split horizontally" })
+keymap.set("n", "<leader>v", "<C-w>v", { desc = "Split vertically" })
+keymap.set("n", "<leader>h", "<C-w>s", { desc = "Split horizontally" })
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close split" })
 
 keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to upper split" })
 keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to split below" })
 keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to split right" })
 keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to split left" })
-keymap.set("n", "-", "<cmd>Oil<CR>", { desc = "Open oil.nvim" })
+keymap.set("n", "<leader>e", ":25Lex<CR>", { desc = "Open netrw" })
+
+keymap.set("i", "'", "''<left>")
+keymap.set("i", '"', '""<left>')
+keymap.set("i", "(", "()<left>")
+keymap.set("i", "{", "{}<left>")
+keymap.set("i", "/**", "/** */<left><left><left>")
+
